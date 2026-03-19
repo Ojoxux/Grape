83d8a881c3508eac0ef5831e155a85e73d57b87f
Jou Okuyama <ojoxux@gmail.com>
2026-03-19 15:37:50 +0900

    entrypoint-dev.shの中身をDockerfile.devに移動した

--- entrypoint-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# 1) Gradle continuous build: watches src/ and recompiles .class files on change
-./gradlew classes --continuous --no-daemon &
-
-# 2) bootRun: DevTools detects .class changes and auto-restarts
-./gradlew bootRun --no-daemon
